67e4130ed82002e4b52504acd3d1e77ac903db88
yash-mewada <yashmewada27@gmail.com>
2023-12-20 14:15:48 +0530

    clerk authentication implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
-  return <Button>hello</Button>;
+  return (
+    <div className="flex flex-col gap-y-4">
+      <h1>Dashboard</h1>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }

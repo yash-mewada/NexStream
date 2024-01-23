a7b9b6b65c98dc45c071b10804aa49583a40505f
yash-mewada <yashmewada27@gmail.com>
2024-01-23 15:10:57 +0530

    create creator dashboard

---
+++ app/(dashboard)/u/[username]/(home)/page.tsx
@@ -0,0 +1,5 @@
+const CreatorPage = () => {
+  return <div>Creator page</div>;
+};
+
+export default CreatorPage;
--- lib/auth-service.ts
+++ lib/auth-service.ts
@@ -20,3 +20,25 @@ export const getSelf = async () => {
 
   return user;
 };
+
+export const getSelfByUsername = async (username: string) => {
+  const self = await currentUser();
+
+  if (!self || !self.username) {
+    throw new Error("Unauthorized");
+  }
+
+  const user = await db.user.findUnique({
+    where: { username },
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  if (self.username !== user.username) {
+    throw new Error("Unauthorized");
+  }
+
+  return user;
+};

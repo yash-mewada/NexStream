1eaf370a6575687ed2e52d5894792c9833464c33
yash-mewada <yashmewada27@gmail.com>
2024-01-24 16:49:05 +0530

    create stream model

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -57,6 +57,11 @@ export async function POST(req: Request) {
         externalUserId: payload.data.id,
         username: payload.data.username,
         imageUrl: payload.data.image_url,
+        stream: {
+          create: {
+            name: `${payload.data.username}'s stream`,
+          },
+        },
       },
     });
   }

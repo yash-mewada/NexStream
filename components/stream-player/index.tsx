819af56221d9d2ae228a08e10f6e0d9208113250
yash-mewada <yashmewada27@gmail.com>
2024-02-12 14:53:52 +0530

    create video component for creator dashboard

---
+++ components/stream-player/index.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useViewerToken } from "@/hooks/use-viewer-token";
+import { Stream, User } from "@prisma/client";
+import { LiveKitRoom } from "@livekit/components-react";
+import { Video } from "./video";
+
+interface StreamPlayerProps {
+  user: User & { stream: Stream | null };
+  stream: Stream;
+  isFollowing: boolean;
+}
+
+export const StreamPlayer = ({
+  user,
+  stream,
+  isFollowing,
+}: StreamPlayerProps) => {
+  const { token, name, identity } = useViewerToken(user.id);
+
+  if (!token || !name || !identity) {
+    return <div>Cannot watch the stream</div>;
+  }
+  return (
+    <>
+      <LiveKitRoom
+        token={token}
+        serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
+        className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full"
+      >
+        <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
+          <Video hostName={user.username} hostIdentity={user.id} />
+        </div>
+      </LiveKitRoom>
+    </>
+  );
+};
---
+++ components/stream-player/video.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { ConnectionState, Track } from "livekit-client";
+import {
+  useConnectionState,
+  useRemoteParticipant,
+  useTracks,
+} from "@livekit/components-react";
+import { OfflineVideo } from "./offline-video";
+import { LoadingVideo } from "./loading-video";
+import { LiveVideo } from "./live-video";
+
+interface VideoProps {
+  hostName: string;
+  hostIdentity: string;
+}
+
+export const Video = ({ hostName, hostIdentity }: VideoProps) => {
+  const connectionState = useConnectionState();
+  const participant = useRemoteParticipant(hostIdentity);
+  const tracks = useTracks([
+    Track.Source.Camera,
+    Track.Source.Microphone,
+  ]).filter((track) => track.participant.identity === hostIdentity);
+
+  let content;
+
+  if (!participant && connectionState === ConnectionState.Connected) {
+    content = <OfflineVideo username={hostName} />;
+  } else if (!participant || tracks.length === 0) {
+    content = <LoadingVideo label={connectionState} />;
+  } else {
+    content = <LiveVideo participant={participant} />;
+  }
+
+  return <div className="aspect-video border-b group relative">{content}</div>;
+};

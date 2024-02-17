9767b01722df10708549a9ef427ab7d82bc3f2c0
yash-mewada <yashmewada27@gmail.com>
2024-02-17 20:13:53 +0530

    create user header for creator dashboard

---
+++ components/stream-player/actions.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useAuth } from "@clerk/nextjs";
+import { Button } from "../ui/button";
+import { Heart } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { onFollow, onUnfollow } from "@/actions/follow";
+import { useTransition } from "react";
+import { toast } from "sonner";
+import { Skeleton } from "../ui/skeleton";
+
+interface ActionsProps {
+  hostIdentity: string;
+  isFollowing: boolean;
+  isHost: boolean;
+}
+
+export const Actions = ({
+  hostIdentity,
+  isFollowing,
+  isHost,
+}: ActionsProps) => {
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+  const { userId } = useAuth();
+
+  const handleFollow = () => {
+    startTransition(() => {
+      onFollow(hostIdentity)
+        .then((data) =>
+          toast.success(`You are now following ${data.following.username}`)
+        )
+        .catch(() => toast.error("Something went wrong"));
+    });
+  };
+
+  const handleUnfollow = () => {
+    startTransition(() => {
+      onUnfollow(hostIdentity)
+        .then((data) =>
+          toast.success(`You have unfollowed ${data.following.username}`)
+        )
+        .catch(() => toast.error("Something went wrong"));
+    });
+  };
+
+  const toggleFollow = () => {
+    if (!userId) {
+      return router.push("/sign-in");
+    }
+
+    if (isHost) return;
+
+    if (isFollowing) {
+      handleUnfollow();
+    } else {
+      handleFollow();
+    }
+  };
+
+  return (
+    <Button
+      disabled={isPending || isHost}
+      onClick={toggleFollow}
+      variant="primary"
+      className="w-full lg:w-auto"
+    >
+      <Heart
+        className={cn("h-4 w-4 mr-2", isFollowing ? "fill-white" : "fill-none")}
+      />
+      {isFollowing ? "Unfollow" : "Follow"}
+    </Button>
+  );
+};
+
+export const ActionsSkeleton = () => {
+  return <Skeleton className="h-10 w-full lg:w-24" />;
+};
--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -8,6 +8,7 @@ import { cn } from "@/lib/utils";
 import { Video, VideoSkeleton } from "./video";
 import { Chat, ChatSkeleton } from "./chat";
 import { ChatToggle } from "./chat-toggle";
+import { Header, HeaderSkeleton } from "./header";
 
 interface StreamPlayerProps {
   user: User & { stream: Stream | null };
@@ -44,6 +45,14 @@ export const StreamPlayer = ({
       >
         <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
           <Video hostName={user.username} hostIdentity={user.id} />
+          <Header
+            hostName={user.username}
+            hostIdentity={user.id}
+            viewerIdentity={identity}
+            imageUrl={user.imageUrl}
+            isFollowing={isFollowing}
+            name={stream.name}
+          />
         </div>
         <div className={cn("col-span-1", collapsed && "hidden")}>
           <Chat
@@ -66,7 +75,7 @@ export const StreamPlayerSkeleton = () => {
     <div className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full">
       <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
         <VideoSkeleton />
-        {/*TODO: Header skeleton */}
+        <HeaderSkeleton />
       </div>
       <div className="col-span-1 bg-background">
         <ChatSkeleton />

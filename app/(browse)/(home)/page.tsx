2d87df9ecf15fcceabab2052ae31f70d59eb32cd
yash-mewada <yashmewada27@gmail.com>
2024-02-21 19:08:49 +0530

    create home page

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,7 +1,13 @@
-export default function Home() {
+import { Suspense } from "react";
+
+import { Results, ResultsSkeleton } from "./_components/results";
+
+export default function Page() {
   return (
-    <div className="flex flex-col gap-y-4">
-      <h1>Home page</h1>
+    <div className="h-full p-8 max-w-screen-2xl mx-auto">
+      <Suspense fallback={<ResultsSkeleton />}>
+        <Results />
+      </Suspense>
     </div>
   );
-}
+};
